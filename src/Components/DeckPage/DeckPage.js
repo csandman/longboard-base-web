ce3d49ba150d56e4de0db0300cd1c3ea508a9000
csandman <csandvik@uvm.edu>
2018-10-23 03:30:16 -0400

    removed unused dependancy

--- src/Components/DeckPage/DeckPage.js
+++ src/Components/DeckPage/DeckPage.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import JsxParser from "react-jsx-parser";
 import { getDeck } from "../../actions/deckActions";
 import "./DeckPage.scss";
 
