0deaddd340c72f6b981d8fa08e736361ff68b064
csandman <csandvik@uvm.edu>
2018-10-24 02:00:52 -0400

    added some media queries for mobile

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,48 +1,101 @@
-import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
-import { FaSearch, FaBars } from 'react-icons/fa';
-import './NavBar.scss'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { FaSearch, FaBars } from "react-icons/fa";
+import { MdClose } from "react-icons/md";
+import "./NavBar.scss";
+var classNames = require("classnames");
 
 export default class NavBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchOpen: false
+    };
+
+    this.openSearch = this.openSearch.bind(this);
+    this.closeSearch = this.closeSearch.bind(this);
+  }
+
+  openSearch() {
+    this.setState({
+      searchOpen: true
+    });
+    document.querySelector(".search-bar>.search").focus();
+  }
+
+  closeSearch() {
+    this.setState({
+      searchOpen: false
+    });
+  }
 
   render() {
+    var searchClass = classNames({
+      "search-bar": true,
+      open: this.state.searchOpen
+    });
+
     return (
       <nav id="nav-bar" className="fixed">
         <div className="container">
           <ul className="left">
             <li className="title-button">
-              <Link to="/" className="link menu-item">LB Base</Link>
+              <Link to="/" className="link menu-item">
+                LB Base
+              </Link>
             </li>
             <li className="search-container">
               <span className="menu-item search-parent">
-                <input type="text" className="search" id="dropdownInput" placeholder="Search..."/>
+                <input
+                  type="text"
+                  className="search"
+                  id="dropdownInput"
+                  placeholder="Search..."
+                />
               </span>
-              <ul id="dropdown"></ul>
+              <ul id="dropdown" />
             </li>
             <li>
-              <Link to="/search" className="link menu-item">Advanced Search</Link>
+              <Link to="/search" className="link menu-item">
+                Advanced Search
+              </Link>
             </li>
           </ul>
           <ul className="right">
             <li>
-              <Link to="/decks" className="link menu-item">Deck Database</Link>
+              <Link to="/decks" className="link menu-item">
+                Deck Database
+              </Link>
             </li>
             <li>
-              <Link to="/add-deck" className="link menu-item">Add a Board</Link>
+              <Link to="/add-deck" className="link menu-item">
+                Add a Board
+              </Link>
             </li>
             <li>
-              <Link to="/guides" className="link menu-item">Guides</Link>
+              <Link to="/guides" className="link menu-item">
+                Guides
+              </Link>
             </li>
           </ul>
           <ul className="mobile">
-            <li className="menu-icon">
-              <FaBars />
-            </li>
             <li className="title-button">
-              <Link to="/" className="link menu-item">LB Base</Link>
+              <Link to="/" className="link menu-item">
+                LB Base
+              </Link>
             </li>
-            <li className="search-icon">
-              <FaSearch />
+            <li className="menu-icon">
+              <FaBars size={20} />
+            </li>
+            <li className="search-icon" onClick={this.openSearch}>
+              <FaSearch size={20} />
+            </li>
+            <li className={searchClass}>
+              <input type="text" className="search" placeholder="Search..." />
+              <div className="close-icon" onClick={this.closeSearch}>
+                <MdClose size={30} />
+              </div>
             </li>
           </ul>
         </div>
