935fbf704c54494f8bdcd57ef1772eb908d770ea
csandman <csandvik@uvm.edu>
2018-10-24 02:12:15 -0400

    added workaround for deployed website not working without redux devtools

--- src/store.js
+++ src/store.js
@@ -6,9 +6,11 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, compose(
-  applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+const composeEnhancers = typeof window === 'object' && window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ ?
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+const composingMiddlewareAndDevTools = composeEnhancers(applyMiddleware(...middleware));
+
+const store = createStore(rootReducer, initialState, composingMiddlewareAndDevTools);
 
 export default store;
